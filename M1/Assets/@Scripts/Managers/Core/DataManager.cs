ddc13ea8dfae0d0069e2f090d2cf2ba381259855
ysy <boneoflily@naver.com>
2024-01-15 22:18:58 +0900

    1-3. Data
    DataMngr , SoundMngr 작업

--- M1/Assets/@Scripts/Managers/Core/DataManager.cs
+++ M1/Assets/@Scripts/Managers/Core/DataManager.cs
@@ -1,16 +1,26 @@
+using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DataManager 
 {
-    void Start()
+    public interface ILoader<Key, Value>
     {
-        
+        Dictionary<Key, Value> MakeDict();
     }
 
-    void Update()
-    {
-        
-    }
+	public Dictionary<int, Data.TestData> TestDic { get; private set; } = new Dictionary<int, Data.TestData>();
+
+	public void Init()
+	{
+		TestDic = LoadJson<Data.TestDataLoader, int, Data.TestData>("TestData").MakeDict();
+	}
+
+	private Loader LoadJson<Loader, Key, Value>(string path) where Loader : ILoader<Key, Value>
+	{
+		TextAsset textAsset = Managers.Resource.Load<TextAsset>(path);
+		return JsonConvert.DeserializeObject<Loader>(textAsset.text);
+	}
 }
