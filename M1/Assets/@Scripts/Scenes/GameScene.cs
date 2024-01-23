14d71c10ed4af23316114aae222736114d111e7e
ysy <boneoflily@naver.com>
2024-01-23 11:09:40 +0900

    3-1. Data #1
    몬스터랑 히어로 프리팹에 각각 스크립트 추가해주니 에러 없어짐

--- M1/Assets/@Scripts/Scenes/GameScene.cs
+++ M1/Assets/@Scripts/Scenes/GameScene.cs
@@ -17,7 +17,7 @@ public class GameScene : BaseScene
 		map.transform.position = Vector3.zero;
 		map.name = "@BaseMap";
 
-		Hero hero = Managers.Object.Spawn<Hero>(new Vector3Int(-10, -5, 0));
+		Hero hero = Managers.Object.Spawn<Hero>(new Vector3Int(-10, -5, 0), HERO_KNIGHT_ID);
 		hero.CreatureState = ECreatureState.Move;
 
 		CameraController camera =  Camera.main.GetOrAddComponent<CameraController>();
@@ -26,8 +26,8 @@ public class GameScene : BaseScene
 		Managers.UI.ShowBaseUI<UI_Joystick>();
 
         {
-            Monster monster = Managers.Object.Spawn<Monster>(new Vector3Int(0, 1, 0));
-            monster.CreatureState = ECreatureState.Idle;
+            Managers.Object.Spawn<Monster>(new Vector3Int(0, 1, 0), MONSTER_BEAR_ID);
+			//Managers.Object.Spawn<Monster>(new Vector3Int(1, 1, 0), MONSTER_SLIME_ID);
         }
 
         // TODO
